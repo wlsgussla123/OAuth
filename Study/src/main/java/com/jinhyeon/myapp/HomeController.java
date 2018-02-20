9545825a10589da7401d2950e3c9234db41a0888
Jinhyeon Park <wlsgussla123@gmail.com>
2018-02-20 16:04:19 +0900

    facebook 계정 접근허용

---
+++ Study/src/main/java/com/jinhyeon/myapp/HomeController.java
@@ -0,0 +1,51 @@
+package com.jinhyeon.myapp;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	private static final String FACEBOOK_CLIENT_ID = "184138445684067";
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value ="/facebookSignin")
+	public String getFacebookSigninCode(HttpSession session) {
+		System.out.println("facebook");
+		
+		String facebookUrl = "https://www.facebook.com/v2.12/dialog/oauth?"
+				+ "client_id="+FACEBOOK_CLIENT_ID+
+				"&redirect_uri="+"http://localhost:8080/myapp";
+
+		return "redirect:"+facebookUrl;
+	}
+}
---
+++ Study/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+<a href="facebookSignin">
+	<button type="button" class="btn btn-primary">Facebook 로그인</button>
+</a>
+</body>
+</html>
